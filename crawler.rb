ddf1c7e897e6e497152a87c8337d6a97efb594ef
npl22 <nli22@jhu.edu>
2017-07-21 16:13:29 -0700

    Make crawler for valid/invalid links

--- crawler.rb
+++ crawler.rb
@@ -3,3 +3,22 @@ require 'open-uri'
 require 'rest-client'
 
 doc = Nokogiri::HTML(open("https://scottduane.github.io/TopSecretClue/"))
+
+links = doc.css('a')
+hrefs = links.map { |link| link.attribute('href').to_s }
+
+p hrefs
+
+valid_links = []
+hrefs.each do |href|
+  puts href
+  begin
+    response = RestClient.get href
+    if response.code == 200
+      valid_links << href
+      puts "FOUND A VALID LINK"
+    end
+  rescue
+    next
+  end
+end
